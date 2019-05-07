af400b454c25cd0aa363c95914bbc31b56efd75b
Markus Sanders <markus.sanderst@gmail.com>
2019-05-07 22:56:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,19 +1,19 @@
 # require modules here
 
-def load_library(file_path)
-  emos = YAML.load_file("lib/emoticons.yml")
-  output = {
-    "get_meaning" => {},
-    "get_emoticon" => {},
-  }
-  emos.each { |meaning, emo|
-    english = emo[0]
-    japanese = emo[1]
-    output["get_meaning"][japanese] = meaning
-    output["get_emoticon"][english] = japanese
-  }
-  output
-end
+# def load_library(file_path)
+#   emos = YAML.load_file("lib/emoticons.yml")
+#   output = {
+#     "get_meaning" => {},
+#     "get_emoticon" => {},
+#   }
+#   emos.each { |meaning, emo|
+#     english = emo[0]
+#     japanese = emo[1]
+#     output["get_meaning"][japanese] = meaning
+#     output["get_emoticon"][english] = japanese
+#   }
+#   output
+# end
 
 def get_japanese_emoticon
   # code goes here
