37bd795996ae7de9c2d1c11ecde9bfea6ded1dda
Markus Sanders <markus.sanderst@gmail.com>
2019-05-07 23:04:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(file, emo)
   dict["get_emoticon"][emo].nil? ? "Sorry, that emoticon was not found" : dict["get_emoticon"][emo]
 end
 
-def get_english_meaning()
+def get_english_meaning(file, emo)
   dict = load_library(file)
   dict["get_meaning"][emo].nil? ? "Sorry, that emoticon was not found" : dict["get_meaning"][emo]
 end
